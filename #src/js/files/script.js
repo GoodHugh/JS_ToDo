31742574eac2f3198220813d6b3ce2a75a577cbb
Nazari <nazari-adamenko@yandex.ru>
2021-11-26 00:29:11 +0300

    удаление элементов списка

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -11,9 +11,26 @@ function innerDiv() {
         newDev.setAttribute('class', 'List');
         document.getElementById('test').append(newDev); 
         newDev.innerText = 'text';
+        let checkBox = document.createElement('input');
+        checkBox.setAttribute('id', 'checkbox' + count);
+        checkBox.setAttribute('class', 'checkbox');
+        checkBox.setAttribute('type', 'checkbox');
+        document.getElementById('row'+ count).append(checkBox);
         arr.push(newDev);
         count++;
     }
+}
+
+function listToDo(){
+    let a = document.querySelectorAll('.checkbox');
+    let d = document.querySelectorAll('.List');
+    a.forEach((elem, i)=> {
+        let b = elem.checked;
+        console.log(b);
+        if (b) {
+            elem.remove();
+        }
+    })
 }
 
 
