d1b3410be535512aa101df4b2f46b6a9e34e3655
Nazari <nazari-adamenko@yandex.ru>
2021-11-28 18:30:45 +0300

    добавление по нажатию Enter

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -2,32 +2,43 @@
 
 let arr = [];
 let ListDel = [];
+const buttonEvent = document.getElementById('add-batton');
+const textareaEvent = document.getElementById('ToDo_value');
 
-document.getElementById('add-batton').addEventListener('click', innerDiv());
+
+buttonEvent.addEventListener('click', innerDiv());
+textareaEvent.addEventListener("keyup", function(event){
+    if (event.code === "Enter") {
+        document.getElementById('add-batton').click();
+    }
+})
+
 
 function innerDiv() {
     let count = 0;
     return function () {
         let taskToDO = document.getElementById('ToDo_value').value;
-        let newDev = document.createElement('div');
-        newDev.setAttribute('id', 'row' + count);
-        newDev.setAttribute('class', 'List');
-        document.getElementById('list_ToDo').append(newDev); 
-        newDev.innerText = taskToDO;
-        document.getElementById('ToDo_value').value = '';
-        let checkBox = document.createElement('input');
-        checkBox.setAttribute('id', 'checkbox' + count);
-        checkBox.setAttribute('class', 'checkbox');
-        checkBox.setAttribute('type', 'checkbox');
-        document.getElementById('row'+ count).append(checkBox);
-        arr.push(newDev);
-        count++;
+        if (taskToDO) {
+            let newDev = document.createElement('div');
+            newDev.setAttribute('id', 'row' + count);
+            newDev.setAttribute('class', 'List');
+            document.getElementById('list_ToDo').append(newDev);
+            newDev.innerText = taskToDO;
+            let checkBox = document.createElement('input');
+            checkBox.setAttribute('id', 'checkbox' + count);
+            checkBox.setAttribute('class', 'checkbox');
+            checkBox.setAttribute('type', 'checkbox');
+            document.getElementById('row' + count).prepend(checkBox);
+            arr.push(newDev);
+            count++;
+            document.getElementById('ToDo_value').value = '';
+        }
     }
 }
 
-function listToDo(){
+function listToDo() {
     const ToDoList = document.querySelectorAll('.List');
-    ToDoList.forEach((elem)=> {
+    ToDoList.forEach((elem) => {
         let elem_check = elem.querySelector('.checkbox').checked;
         if (elem_check) {
             ListDel.push(elem);
